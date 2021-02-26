9fa433002fac4b6f394ba81ef64b4d9f1012551a
“Chand” <18bce249@nirmauni.ac.in>
2021-02-26 18:00:07 +0530

    small changes

--- videoster/app.js
+++ videoster/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+
+const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const auth = require("./routes/auth");
 const user = require("./routes/users");
 
