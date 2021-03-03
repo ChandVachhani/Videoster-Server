8770335967ad07c2448febb35e098072ff3ad8b5
“Chand” <18bce249@nirmauni.ac.in>
2021-03-03 21:46:55 +0530

    changes

--- videoster/utils/database.js
+++ videoster/utils/database.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   host: "ec2-34-203-155-237.compute-1.amazonaws.com",
-  port: process.env.PORT,
+  port: 5432,
   dialectOptions: {
     ssl: {
       require: true,
