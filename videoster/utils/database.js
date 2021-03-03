d494ffc7b6ecfd1a1d1cbe25523fa4cc28f0a061
“Chand” <18bce249@nirmauni.ac.in>
2021-03-03 23:16:35 +0530

    Revert "Merge branch 'UI' into deploy"
    This reverts commit d96db1978e26580c39abddb15e1391a6a53f034d, reversing
    changes made to 7186be9b155b23955037ac68751618a35cdd51e6.

--- videoster/utils/database.js
+++ videoster/utils/database.js
@@ -1,8 +1,16 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("Videoster", "root", "chand", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  host: "ec2-34-203-155-237.compute-1.amazonaws.com",
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
