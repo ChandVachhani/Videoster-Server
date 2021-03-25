d353c9ddcaac943b038a3a6da7311fb2f266bc2d
“Chand” <18bce249@nirmauni.ac.in>
2021-03-25 15:27:23 +0530

    gug solved

--- videoster/routes/YT.js
+++ videoster/routes/YT.js
@@ -6,5 +6,6 @@ const ytController = require("../controllers/ytController");
 router.get("/channels/:searchword", ytController.getChannels);
 router.get("/channel/:channelId", ytController.getChannel);
 router.get("/channels/:channelId/videos", ytController.getVideos);
+router.get("/videos/:videoId", ytController.getVideo);
 
 module.exports = router;
