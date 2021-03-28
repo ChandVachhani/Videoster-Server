e53f64a79c756182f9f5cffcbb561e85d8867ec0
“Chand” <18bce249@nirmauni.ac.in>
2021-03-28 23:21:43 +0530

    small change

--- videoster/apis/YT.js
+++ videoster/apis/YT.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-const MY_KEY = "AIzaSyA1KurVPfdRyRJfcs3820IoIAALjJPE-VY";
 const KEY = "AIzaSyAgnnwHjqsxd0u1HdlRXQyLmZ5wDznKOhM";
+const MY_KEY = "AIzaSyA1KurVPfdRyRJfcs3820IoIAALjJPE-VY";
 
 exports.YT = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
