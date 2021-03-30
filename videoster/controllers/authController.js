55ab442b19ca5fb3a074d058ff64d12654c69b01
“Chand” <18bce249@nirmauni.ac.in>
2021-03-30 22:38:39 +0530

    forgot password added

--- videoster/controllers/authController.js
+++ videoster/controllers/authController.js
@@ -11,6 +11,31 @@ const bcrype = require("bcrypt");
 const UIDGenerator = require("uid-generator");
 const uidgen = new UIDGenerator();
 
+////////// MAILGUN /////////////
+
+const mailgun = require("mailgun-js");
+const DOMAIN = "videoster.tech";
+const mg = mailgun({
+  apiKey: "987ca0c6b61848e8be31ba7015321b6c-b6d086a8-22e3f7f7",
+  domain: DOMAIN,
+});
+
+sendMail = function (reciever_email, email_subject, email_body) {
+  const data = {
+    from: "Videoster <developer@videoster.tech>",
+    to: reciever_email,
+    subject: email_subject,
+    text: email_body,
+  };
+
+  mg.messages().send(data, (error, body) => {
+    if (error) console.log(error);
+    else console.log(body);
+  });
+};
+
+////////////////////////////////
+
 exports.Login = async (req, res, next) => {
   const { userName, password } = req.body;
   try {
@@ -76,6 +101,61 @@ exports.Register = async (req, res, next) => {
     console.log(err);
     res.status(401).json({
       message: "userName or email already Exist",
+    });
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const { userName } = req.body;
+    const user = await users.findOne({
+      where: {
+        [Op.or]: [{ email: userName }, { userName: userName }],
+      },
+    });
+    if (!user) {
+      res.status(401).json({
+        message: "userName of email not Found!",
+      });
+    }
+    const email = user.dataValues.email;
+    const content = `http://localhost:3000/changePassword/${user.dataValues.token}`;
+    sendMail(email, "Change Password", content);
+    res.status(200).json({
+      message: "Check your mail!",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      message: "Email id or userName does not exist!",
+    });
+  }
+};
+
+exports.changePassword = async (req, res, next) => {
+  try {
+    const { password, token } = req.body;
+    const user = await users.findOne({
+      where: {
+        token,
+      },
+    });
+    if (!user) {
+      res.status(401).json({
+        message: "token is Invalid!",
+      });
+    }
+    const hashedPassword = await bcrype.hash(password, 12);
+    await user.update({
+      password: hashedPassword,
+    });
+    res.status(200).json({
+      message: "password changed successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      message: "some error in changing pass!",
     });
   }
 };
--- videoster/routes/auth.js
+++ videoster/routes/auth.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/Login", authController.Login);
-
 router.post("/Register", authController.Register);
+router.post("/forgotPassword", authController.forgotPassword);
+router.post("/changePassword", authController.changePassword);
 
 module.exports = router;
