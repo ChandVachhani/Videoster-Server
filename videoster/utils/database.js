8590da83e4deda5589951a2e01023c7af27e8de8
ChandVachhani <chandvachhani4441@gmail.com>
2021-07-17 23:38:49 +0530

    testing API

--- videoster/utils/database.js
+++ videoster/utils/database.js
@@ -1,16 +1,9 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  host: "ec2-34-203-155-237.compute-1.amazonaws.com",
-  port: 5432,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+const sequelize = new Sequelize("Videoster", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
 });
+
 
 module.exports = sequelize;
